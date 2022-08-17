c78b8f5de1938616f0e2364a55b58f45d3a3b9a9
Justyna-Adamska <110631335+Justyna-Adamska@users.noreply.github.com>
2022-08-17 20:22:47 +0200

    Program counting negative numbers

---
+++ liczbyujemne.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main ()
+
+{
+	int8_t z1 = 127;
+	int8_t z2 = 1;
+	 printf("%d\t %x\n", z1,z1);
+z1=z1+z2;
+printf("%d\t%X\n", z1, z1);
+	 return 0;
+}
