b3d420c2656160af21a7828abb5d329073d3004f
Justyna <justyna.anna.adamska@gmail.com>
2022-09-06 21:50:56 +0200

    Modified program with functions (introducing for loop)

--- linear_function_2_param.c
+++ linear_function_2_param.c
@@ -13,9 +13,58 @@ return;
 
 }
 
+void linear_range (int a, int b) { /*by coincidence same names as in line 5 - they are parameters of another function*/
+
+	printf("Values f(x) for the range <%d, %d>:\n", a,b);
+
+return;
+}
+
+int linear_values (int a, int x, int b){
+
+
+	int result = a*x+b;
+	
+
+
+return result;
+
+}
+
+void edit_function (int a, int x, int b){
+
+	int wynik = linear_values (a,x,b);
+
+	printf("f(%d)=%d\n", x, wynik);
+
+return;
+
+}
+
+void function_loop (int a, int b, int lower, int upper) {
+
+
+	int x;
+
+	for (x=lower; x<=upper; ++x) {
+
+	edit_function(a,x,b);
+
+}
+
+return;
+}
+
 int main (){
 
 	linear_2_param (2,6);
 
+	linear_range (0,2);
+	
+	linear_values(2,0,6);
+	
+//	edit_function(2,0,6);	
+
+	function_loop (2,6,0,2);	
 return 0;
 }
